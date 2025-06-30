9d1b6507c7c90c8a89ba6652fbd57c505702de82
Samar Ali <samarali27698@gmail.com>
2025-06-30 20:00:48 +0300

    Added display_info, mark_adopted, and birthday methods to pet class

--- pet_tracker.py
+++ pet_tracker.py
@@ -5,4 +5,25 @@ class pet:
         self.age=age
         self.adopted=False
     
+    def display_info(self):
+        if self.adopted==True:
+            adoption_status="is adopted"
+        else:
+            adoption_status="Not adopted"
+        print(f"Pet's name is : {self.name}", 
+              f"Pet's age is : {self.age}" ,
+              f"Pet's specie is: {self.species}",
+              f"Pet's adoption status is :{adoption_status}"
+        )
+
+    def mark_adopted(self):
+        self.adopted==True
+        print(f"{self.adopted} is now adopted")    
+
+    def birthday(self):
+        self.age+=1
+        print(f"Happy birthday{self.name}, your age is: {self.age}")
+
+
     
+
