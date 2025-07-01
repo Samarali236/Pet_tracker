b6cf58ebbe28f98d44acbb047f75f2d1ef817bfd
Samar Ali <samarali27698@gmail.com>
2025-07-01 03:18:10 +0300

    Created manual pet list and added function to find non-adopted pets

--- pet_tracker.py
+++ pet_tracker.py
@@ -1,44 +1,50 @@
+#part1
 class pet:
-    def __init__(self,name,species,age):
+    def __init__(self,name,species,age,adopted=False):
         self.name=name
         self.species=species
         self.age=age
-        self.adopted=False
+        self.adopted=adopted
     
     def display_info(self):
         if self.adopted==False:
             adoption_status="is not adopted"
         else:
             adoption_status="is adopted"
-        print(f"Pet's name is : {self.name} \n", 
-              f"Pet's age is : {self.age} \n" ,
-              f"Pet's specie is: {self.species} \n",
-              f"Pet's adoption status is : {adoption_status}"
-        )
+        return f"Pets name is : "+ self.name +" , Pets specie is: "+self.species +" , Pets age is :"+str(self.age) +" ,Pet : "+ adoption_status
 
     def mark_adopted(self):
         self.adopted=True
-        print(f"{self.name} is now adopted")    
+        return f"The pet {self.name} is now adopted"   
 
     def birthday(self):
         self.age+=1
-        print(f"Happy birthday {self.name}, your age is: {self.age}")
+        return f" Happy birthday {self.name}! \n your age now is : {self.age}"
+
 
 pet1=pet("Max", "Dog", 1)
 pet2=pet("Molly","Cat", 2)
 pet3=pet("Flufy","Cat", 3)
 pet4=pet("Barney","Dog", 2)
 
+print(pet1.display_info())
+print(pet1.birthday())
+print(pet1.mark_adopted())
+print(pet1.display_info())
 
-pet1.display_info()
-#pet2.display_info()
-#pet3.display_info()
-#pet4.display_info()
+#part2
+pets_list = [pet1, pet2, pet3, pet4]
+
+def find_non_adopted(list):
+    non_adopted_list = []
+    for pet in list:
+        if pet.adopted == False:
+            non_adopted_list.append(pet.display_info())
+    return f"The List of non adopted pets is {non_adopted_list}"
+
+print(find_non_adopted(pets_list))
 
 
-pet1.birthday()
-pet1.mark_adopted()
-pet1.display_info()
 
 
     
